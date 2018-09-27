83db47251243a4b2ba26a69d202fd4d162709a09
wang <wang@HankeMBP.local>
2018-09-27 18:05:31 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wang/hkumoodle/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.wang.hkumoodle;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        if (v.getId() == R.id.btn_Login) {
+            String uname = txt_UserName.getText().toString();
+            String upassword = txt_UserPW.getText().toString();
+
+            System.out.println( "@@@@@@@@@@@@@@@\n" +
+                    "The Portal ID is: " + uname + "\n" +
+                    "The Password is: " + upassword + "\n" +
+                    "@@@@@@@@@@@@@@@" );
+        }
+    }
+
+    EditText txt_UserName, txt_UserPW;
+    Button btn_Login;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_Login = (Button)findViewById(R.id.btn_Login);
+        txt_UserName = (EditText)findViewById(R.id.txt_UserName);
+        txt_UserPW = (EditText)findViewById(R.id.txt_UserPW);
+        // Register the Login button to click listener
+        // Whenever the button is clicked, onClick is called
+        btn_Login.setOnClickListener(this);
+    }
+}
