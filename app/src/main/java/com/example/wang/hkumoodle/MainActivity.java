254dc4b2dcf42fc23c260179c9c01271e252ec1c
wang <wanghk00a1@outlook.com>
2018-10-03 16:19:56 +0800

    WS2

--- app/src/main/java/com/example/wang/hkumoodle/MainActivity.java
+++ app/src/main/java/com/example/wang/hkumoodle/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.wang.hkumoodle;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -35,6 +38,19 @@ public class MainActivity extends Activity implements View.OnClickListener {
             System.out.println(
                     "The Portal ID is: " + uname + "\n" +
                             "The Password is: " + upassword + "\n");
+
+            Intent intent = new Intent(getBaseContext(), CourseListActivity.class);
+            ArrayList<String> cname = new ArrayList<String>();
+            ArrayList<String> cteachers = new ArrayList<String>();
+//*************Sample Data*************//
+            cname.add("c1"); cteachers.add("t1");
+            cname.add("c2"); cteachers.add("t2");
+            cname.add("c3"); cteachers.add("t3");
+            cname.add("c4"); cteachers.add("t4");
+//*************Sample Data*************//
+            intent.putStringArrayListExtra("CourseName", cname);
+            intent.putStringArrayListExtra("Teachers", cteachers);
+            startActivity(intent);
         }
     }
 }
